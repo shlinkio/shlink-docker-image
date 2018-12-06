8e3b0a401da7aa5ef29ab559eeab1230cb41b59b
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-12-06 19:47:55 +0100

    Fixed container restarting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 cd /etc/shlink
 
 # If proxies have not been generated yet, run first-time operations
-if ! [ $(ls -A data/proxies) ]; then
+if [ -z "$(ls -A data/proxies)" ]; then
     rm -f data/cache/app_config.php
     php vendor/doctrine/orm/bin/doctrine.php orm:schema-tool:create
     php vendor/doctrine/migrations/bin/doctrine-migrations.php migrations:migrate
@@ -12,4 +12,6 @@ if ! [ $(ls -A data/proxies) ]; then
     shlink visit:update-db
 fi
 
-php vendor/zendframework/zend-expressive-swoole/bin/zend-expressive-swoole start
+# When restarting the container, swoole might think it is already in execution
+# This forces the app to be started every second until the exit code is 0
+until php vendor/zendframework/zend-expressive-swoole/bin/zend-expressive-swoole start; do sleep 1 ; done
