00d2a21ec768d2078d7a895a6466dbf7c1668aa4
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-05-13 18:35:13 +0200

    Updated entry point so that it does not try to download GeoLite database

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,9 +14,6 @@ php vendor/doctrine/migrations/bin/doctrine-migrations.php migrations:migrate -n
 echo "Generating proxies..."
 php vendor/doctrine/orm/bin/doctrine.php orm:generate-proxies -n -q
 
-echo "Updating GeoLite2 database..."
-shlink visit:update-db -q
-
 # When restarting the container, swoole might think it is already in execution
 # This forces the app to be started every second until the exit code is 0
 until php vendor/zendframework/zend-expressive-swoole/bin/zend-expressive-swoole start; do sleep 1 ; done
